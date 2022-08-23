cae54bc2bbbf1a2506492600dbe5f54f529e92f1
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-08-23 11:44:18 +0200

    update

--- connect.php
+++ connect.php
@@ -1,10 +1,9 @@
 <?php
     try{
-        $db = new PDO("mysql:host=localhost;dbname=id19341347_online_chat",'id19341347_root','Vb-82tCWnJahNj24');
+        $db = new PDO("Your Host Here",'Username','Password');
         $db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
         
     }
     catch(PDOException $e){
         echo $e->getMessage();
     }
-?>
